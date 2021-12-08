a5ebe7341f8fee5cc6cc6590e5ec23c6077c6f58
SUMIT KUMAR <77963232+sumitkumar16dec@users.noreply.github.com>
2021-12-08 13:53:41 +0530

    Create Program to create random numbers.cpp

---
+++ Program to create random numbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+  int random_number{};
+  size_t count {10};
+  int min {1};
+  int max {6};
+  cout<<"RAND_MAX on my system is: "<< RAND_MAX <<endl;
+  srand(time(nullptr));
+  for(size_t i{1}; i<=count; ++i){
+    random_number = rand() % (max - min + 1) + min;
+    cout<< random_number <<endl;
+  }
+
+  return 0;
+}
